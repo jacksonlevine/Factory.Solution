ee5a6c36f9c6b33e88fefce2f0d694cef65d09d5
jacksonlevine <jack@netbug.us>
2023-03-17 09:52:08 -0700

    WIP: engineer controller, finished homecontroller

---
+++ Factory/Controllers/EngineersController.cs
@@ -0,0 +1,140 @@
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using Factory.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Factory.Controllers
+{
+  public class EngineersController : Controller
+  {
+    private readonly FactoryContext _db;
+
+    public EngineersController(FactoryContext db)
+    {
+      _db = db;
+    }
+    public ActionResult Index()
+    {
+      List<Engineer> model = _db.Engineers
+                            .Include(engineer => engineer.JoinEntities)
+                            .ThenInclude(join => join.Machine)
+                            .ToList();
+      return View(model);
+    }
+    public ActionResult Create()
+    {
+      ViewBag.SpecialtyId = new SelectList(_db.Specialties, "SpecialtyId", "Name");
+      return View();
+    }
+
+    [HttpPost]
+    public ActionResult Create(Doctor doctor)
+    {
+      if (!ModelState.IsValid)
+      {
+        ViewBag.SpecialtyId = new SelectList(_db.Specialties, "SpecialtyId", "Name");
+        return View(doctor);
+      }
+      else
+      {
+        _db.Doctors.Add(doctor);
+        _db.SaveChanges();
+        return RedirectToAction("Index");
+      }
+    }
+
+    public ActionResult Details(int id)
+    {
+      Doctor thisDoctor = _db.Doctors
+          .Include(doctor => doctor.JoinEntities2)
+          .ThenInclude(join => join.Specialty)
+          .Include(doctor => doctor.JoinEntities)
+          .ThenInclude(join => join.Patient)
+          .FirstOrDefault(doctor => doctor.DoctorId == id);
+      return View(thisDoctor);
+    }
+
+    public ActionResult Edit(int id)
+    {
+      Doctor thisDoctor = _db.Doctors.FirstOrDefault(doctor => doctor.DoctorId == id);
+      ViewBag.SpecialtyId = new SelectList(_db.Specialties, "SpecialtyId", "Name");
+      return View(thisDoctor);
+    }
+
+    [HttpPost]
+    public ActionResult Edit(Doctor doctor)
+    {
+      _db.Doctors.Update(doctor);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
+    public ActionResult Delete(int id)
+    {
+      Doctor thisDoctor = _db.Doctors.FirstOrDefault(doctor => doctor.DoctorId == id);
+      return View(thisDoctor);
+    }
+
+    [HttpPost, ActionName("Delete")]
+    public ActionResult DeleteConfirmed(int id)
+    {
+      Doctor thisDoctor = _db.Doctors.FirstOrDefault(doctor => doctor.DoctorId == id);
+      _db.Doctors.Remove(thisDoctor);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
+    public ActionResult AddPatient(int id)
+    {
+      Doctor thisDoctor = _db.Doctors.FirstOrDefault(doctor => doctor.DoctorId == id);
+      ViewBag.PatientId = new SelectList(_db.Patients, "PatientId", "Name");
+      return View(thisDoctor);
+    }
+
+    [HttpPost]
+    public ActionResult AddPatient(Doctor doctor, int patientId)
+    {
+#nullable enable
+      DoctorPatient? joinEntity = _db.DoctorPatients.FirstOrDefault(join => (join.PatientId == patientId && join.DoctorId == doctor.DoctorId));
+#nullable disable
+      if (joinEntity == null && patientId != 0)
+      {
+        _db.DoctorPatients.Add(new DoctorPatient() { PatientId = patientId, DoctorId = doctor.DoctorId });
+        _db.SaveChanges();
+      }
+      return RedirectToAction("Details", new { id = doctor.DoctorId });
+    }
+
+    public ActionResult AddSpecialty(int id)
+    {
+      Doctor thisDoctor = _db.Doctors.FirstOrDefault(doctor => doctor.DoctorId == id);
+      ViewBag.SpecialtyId = new SelectList(_db.Specialties, "SpecialtyId", "Name");
+      return View(thisDoctor);
+    }
+
+    [HttpPost]
+    public ActionResult AddSpecialty(Doctor doctor, int specialtyId)
+    {
+#nullable enable
+      DoctorSpecialty? joinEntity = _db.DoctorSpecialties.FirstOrDefault(join => (join.DoctorId == doctor.DoctorId && join.SpecialtyId == specialtyId));
+#nullable disable
+      if (joinEntity == null && specialtyId != 0)
+      {
+        _db.DoctorSpecialties.Add(new DoctorSpecialty() { DoctorId = doctor.DoctorId, SpecialtyId = specialtyId });
+        _db.SaveChanges();
+      }
+      return RedirectToAction("Details", new { id = doctor.DoctorId });
+    }
+
+    [HttpPost]
+    public ActionResult DeleteJoin(int joinId)
+    {
+      DoctorPatient joinEntry = _db.DoctorPatients.FirstOrDefault(entry => entry.DoctorPatientId == joinId);
+      _db.DoctorPatients.Remove(joinEntry);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+  }
+}
